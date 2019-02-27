bc0732458a4f3bbd9cd3b501a1adba5e9b5ff2e6
Alex Bevziuk <alex.bevziuk@okta.com>
2019-02-27 14:45:09 +0200

    java9 modules

---
+++ java-modules/src/main/java/com/lohika/workshop/modules/Main.java
@@ -0,0 +1,12 @@
+package com.lohika.workshop.modules;
+
+/**
+ * @author Alex Bevziuk
+ * 2019-02-27
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("module test");
+    }
+}
---
+++ java-modules/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Alex Bevziuk
+ * 2019-02-27
+ */
+module workshopOne {
+    requires java.base;
+}
